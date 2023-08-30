9a103df0e95276b4a5905d4104faaf7ac7247b9a
tabby400 <tabbynargaret@gmail.com>
2023-08-30 08:22:02 -0700

    depth of node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function gets the depth
+ *
+ * @tree: points tot he node to get depth of
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+
